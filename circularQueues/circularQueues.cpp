6e8d8cc6a41b1f1436b35428c40292a6d3864671
hi-lufz1 <latifufauzi@gmail.com>
2023-06-23 09:40:07 +0700

    10

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -40,5 +40,20 @@ public:
 
 	}
 
+	void remove() {
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "\nQueue underflow\n";
+			return;
+		}
+		cout << "\nTHE element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
+		// Cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+
+		}
+		else {}
+	}
 };
