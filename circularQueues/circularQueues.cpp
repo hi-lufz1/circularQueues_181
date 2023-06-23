dbc636518eba33327c8fab872e9247ed84dda435
hi-lufz1 <latifufauzi@gmail.com>
2023-06-23 11:07:00 +0700

    17

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -84,6 +84,11 @@ public:
 		}
 		else {
 			//jika Front_position > Rear_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "    ";
+				FRONT_position++;
+			}
+			FRONT_position = 0;
 		}
 	}
 };
