b2c5e1fc1663cbd8f665e2a5b1b4fb402ff118b3
hi-lufz1 <latifufauzi@gmail.com>
2023-06-23 09:33:06 +0700

    7

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -23,5 +23,17 @@ public:
 			cout << "\nQueue overflow\n";
 			return;
 		}
+
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//Jika REAR berada diposisi terakhir array, kembali ke awal array
+			if (REAR == max == -1)
+				REAR = 0;
+
+		}
 	}
 };
