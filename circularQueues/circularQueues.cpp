b69352d0827e6e8f83dbaaeed5fc49e28db434d7
hi-lufz1 <latifufauzi@gmail.com>
2023-06-23 09:42:23 +0700

    11

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -54,6 +54,14 @@ public:
 			REAR = -1;
 
 		}
-		else {}
+		else {
+			// Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
+
+	void
 };
